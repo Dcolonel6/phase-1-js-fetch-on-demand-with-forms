44cb86849c70431013d7ba49bc30ea0f95cf9bfb
frustrated-coder <d.n_mwangi@outlook.com>
2022-08-25 01:08:01 +0300

    finished assignment

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const value = event.target.children[1].value
+        fetch(`http://localhost:3000/movies/${value}`)
+        .then(response => response.json())
+        .then(data => {
+            const title = document.querySelector('section#movieDetails h4');
+            const summary = document.querySelector('section#movieDetails p');
+      
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+
+    });
   
 }
 
